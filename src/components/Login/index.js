4e61ed94c9466df274ea7092b10fe0c81e44a08e
lopezatn <agustinlpzb@gmail.com>
2023-08-08 15:54:25 +0200

    first commit

---
+++ src/components/Login/index.js
@@ -0,0 +1,7 @@
+const Login = () => {
+    return ( <>
+    <p>Lets test this!</p>
+    </> );
+}
+ 
+export default Login;
---
+++ src/components/Register/index.js
@@ -0,0 +1,5 @@
+const Register = () => {
+    return ( <h2>Please register in order to Log-in</h2> );
+}
+ 
+export default Register;
